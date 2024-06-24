0xC0000054/sc4-park-and-ride-ordinance

commit 8692b097f125620a4384126db204a046113ac0ec

Call the SetOn method in the base class

src/ParknRideOrdinance.cpp
@@ -400,9 +400,9 @@ int64_t ParknRideOrdinance::GetCurrentMonthlyIncome()
 
 bool ParknRideOrdinance::SetOn(bool isOn)
 {
+	OrdinanceBase::SetOn(isOn);
 	if (on != isOn)
 	{
-		on = isOn;
 		UpdateCarCanReachDestination(/*calledFromPostCityInit*/false);
 	}
 

commit 5c1e1c475cb5a76558ba05c0956524e537863c9c

Fix enabling and disabling the ordinance
This was broken in 8692b097f125620a4384126db204a046113ac0ec.

src/ParknRideOrdinance.cpp
@@ -400,8 +400,10 @@ int64_t ParknRideOrdinance::GetCurrentMonthlyIncome()
 
 bool ParknRideOrdinance::SetOn(bool isOn)
 {
+	bool oldIsOn = isOn;
+
 	OrdinanceBase::SetOn(isOn);
-	if (on != isOn)
+	if (oldIsOn != isOn)
 	{
 		UpdateCarCanReachDestination(/*calledFromPostCityInit*/false);
 	}